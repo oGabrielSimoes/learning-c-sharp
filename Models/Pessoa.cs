1a12b002a91506d6a4f0be2a963ffab57fd445fa
oGabrielSimoes <gabriel.simoes1997@outlook.com>
2025-06-14 03:15:08 -0300

    testeee

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EXEMPLOSFUNDAMENTOS.Models //Nome do caminho lógico ao qual iremos nos referenciar para usar a classe
+namespace EXEMPLOSFUNDAMENTOS.Models
 {
-    public class Pessoa //Cria uma Classe Pública de nome Pessoa
+    public class Pessoa 
     {
-        public string Nome  { get; set; } //Indica um atributo públido chamado Nome do tipo String com seus metodos get e set.
-        public int Idade { get; set; } //Indica um atributo públido chamado Idade do tipo String com seus metodos get e set.
+        public string Nome { get; set; } 
+        public int Idade { get; set; } 
 
-        public void Apresentar() //Indica a criação de método chamado Apresentar.
+        public void Apresentar() 
         {
-            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos"); //atividade que será executada quando o método for chamado.
-           
+            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos"); 
         }
 
     }
 }
+
+//Na linha 6 temos o Nome do caminho lógico ao qual iremos nos referenciar para usar a classe 
+//Na Linha 8 Criamos uma Classe Pública de nome Pessoa
+//Na linha 10 Criamos um atributo público chamado Nome do tipo String com seus metodos get e set.
+//Na Linha 11 Criamos um atributo público chamado Idade do tipo Int (Inteiro) com seus metodos get e set.
+//Na linha 13 Criamos um método público chamado Apresentar.
+//Na Linha 15 mostra a atividade que será executada quando o método for chamado.
--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
-﻿using EXEMPLOSFUNDAMENTOS.Models; //Indicando o caminho lógico onde se encontra nossa classe chamada Pessoa.
+﻿using EXEMPLOSFUNDAMENTOS.Models;          
 
-Pessoa pessoa1 = new Pessoa(); // Indica que estamos "chamando" a classe Pessoa e nomeando-a como pessoa1 e...
-// instanciando ela com o comando = new Pessoa
+Pessoa pessoa1 = new Pessoa();           
 
-pessoa1.Nome = "Gabriel"; //Indiquei um parâmetro para o atributo Nome, chamando pessoa1(instancia que criamos).Nome(Atributo da classe) 
-pessoa1.Idade = 28; //Indiquei um parâmetro para o atributo Idade, chamando pessoa1(instancia que criamos).Idade(Atributo da classe)
-pessoa1.Apresentar(); // Foi feito a chamada para o método Apresentar da classe pessoa.
+pessoa1.Nome = "Gabriel"; 
+pessoa1.Idade = 28; 
+pessoa1.Apresentar(); 
+
+
+
+
+
+//Indicamos o caminho lógico onde se encontra nossa classe chamada Pessoa na linha 1 
+// Indicamos na linha 3 que estamos "chamando" a classe Pessoa e nomeando-a como pessoa1 e instanciando ela com o comando = new Pessoa;
+//Indiquei um parâmetro para o atributo Nome na linha 5 , chamando pessoa1(instancia que criamos).Nome(Atributo da classe);
+//Indiquei um parâmetro para o atributo Idade na linha 6, chamando pessoa1(instancia que criamos).Idade(Atributo da classe);
+// Foi feito a chamada para o método Apresentar da classe pessoa na linha 7.;
+//Em suma quando quisermos usar uma classe e seus atributos e métodos digitamos o nome da classe como exemplo pessoa1 e usamos o ponto "."
+//junto com o metodo ou atributo que queremos utilizar
+//SEMPRE que usarmos comando devemos utilizar  " ; " ao final. 
